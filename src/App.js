666b7c6b683d8c00c177dea4c5901b29f65f84b3
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-08-31 18:08:42 +0200

    Portal

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 import FetchData from './components/FetchData';
-import ErrorBoundaries from './components/ErrorBoundaries'
+import ErrorBoundaries from './components/ErrorBoundaries';
+import Portal from './components/Portal';
+import Text from './components/Text';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ErrorBoundaries renderOnError={({ errorInfo })=><p>Unexpected error occurred</p>} >
-          <FetchData url='https://randomuser.me/api/?results=4' >
-            {({ data, loading, error, refetch }) => {
-              if(loading) return <p>Loading</p>
-              if(error) return (
-                <div>
-                  <p>Ooops, error occurred</p>
-                  <button onClick={refetch}>Try fetch data again</button>
-                </div>
-              )   
-              if(!data || data.results.length === 0) return null;         
+	state = {
+		message: 'Hello from Portal',
+	};
+	render() {
+		return (
+			<div className="App">
+				<ErrorBoundaries
+					renderOnError={({ errorInfo }) => <p>Unexpected error occurred</p>}
+				>
+					<FetchData url="https://randomuser.me/api/?results=4">
+						{({ data, loading, error, refetch }) => {
+							if (loading) return <p>Loading</p>;
+							if (error)
+								return (
+									<div>
+										<p>Ooops, error occurred</p>
+										<button onClick={refetch}>Try fetch data again</button>
+									</div>
+								);
+							if (!data || data.results.length === 0) return null;
 
-              return  (
-                <div>
-                  {data.results.map(person => (
-                    <li key={person.login.uuid}>
-                      <p>{person.name.first} {person.name.last}</p>
-                    </li>
-                  ))}
-                  <button onClick={refetch}>Refetch</button>
-                </div>
-              )
-            }}
-          </FetchData>
-        </ErrorBoundaries>
-      </div>
-    );
-  }
+							return (
+								<div>
+									{data.results.map(person => (
+										<li key={person.login.uuid}>
+											<p>
+												{person.name.first} {person.name.last}
+											</p>
+										</li>
+									))}
+									<button onClick={refetch}>Refetch</button>
+								</div>
+							);
+						}}
+					</FetchData>
+				</ErrorBoundaries>
+				<Portal>
+					<Text message={this.state.message} />
+				</Portal>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Text.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Text = ({ message }) => <p>{message}</p>;
+
+export default Text;
